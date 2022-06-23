0d8ecf0a1cc1cb4fb2d3d63211a8e7e98f8251c4
Pooja <92075579+ppoojjaa@users.noreply.github.com>
2022-06-23 22:18:34 +0530

    Add files via upload

---
+++ Lesson-5/README.md
@@ -0,0 +1,4 @@
+# Ethers Simple Storage Lesson5 
+This is lesson 5 of @PatrickCollins 32 Blockchain course
+
+Time Stamp: https://www.youtube.com/watch?v=gyMwXuJrbJQ&t=19842s  [5:30:43 - 8:20:18 / 1:07:54:30]
---
+++ Lesson-5/encryptKey.js
@@ -0,0 +1,21 @@
+const ethers = require("ethers");
+const fs = require("fs");
+require("dotenv").config();
+
+async function main() {
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+    const encryptedJsonKey = await wallet.encrypt(
+        process.env.PRIVATE_KEY_PASSWORD,
+        process.env.PRIVATE_KEY
+    );
+    console.log(encryptedJsonKey); // the ouput can only be decrypted by the password
+
+    fs.writeFileSync("./encryptedKey.json", encryptedJsonKey); //saving it to a new file
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
